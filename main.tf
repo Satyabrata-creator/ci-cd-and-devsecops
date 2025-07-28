8a3e5cc11e34987fa0982d1ae99b87dcae4590da
Satyabrata-creator <satyabratam1970@gmail.com>
2025-07-28 10:58:34 +0530

    Modified and corrected

--- main.tf
+++ main.tf
@@ -1,37 +1,131 @@
-module "s3" {
-  source       = "./modules/s3"
-  project_name = var.project_name
-  bucket_name  = var.bucket_name
+# S3 bucket for CodePipeline artifacts
+resource "aws_s3_bucket" "artifact_bucket" {
+  bucket = var.artifact_bucket
+  acl    = "private"
 }
 
-module "iam" {
-  source                 = "./modules/iam"
-  project_name           = var.project_name
-  github_connection_name = "project-git-connection"
-  kms_key_arn            = var.kms_key_arn
+# IAM Role for CodeBuild
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-service-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
-module "codebuild" {
-  source             = "./modules/codebuild"
-  project_name       = var.project_name
-  s3_bucket          = var.bucket_name
-  codebuild_role_arn = module.iam.codebuild_role_arn
+# IAM Role Policy Attachment for CodeBuild
+resource "aws_iam_role_policy_attachment" "codebuild_attach" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
 }
 
-module "codepipeline" {
-  source                 = "./modules/codepipeline"
-  project_name           = var.project_name
-  codestar_arn           = module.iam.github_codestar_connection_arn
-  codebuild_project_name = module.codebuild.codebuild_project_name
-  artifact_bucket        = var.bucket_name
-  pipeline_role_arn      = module.iam.codepipeline_role_arn
+# CodeBuild Project
+resource "aws_codebuild_project" "ci_project" {
+  name          = "devsecops-ci"
+  description   = "Build project for DevSecOps CI/CD"
+  build_timeout = 5
+  service_role  = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yaml"
+  }
 }
 
-module "ec2" {
-  source                = "./modules/ec2"
-  instance_profile_name = module.iam.ec2_instance_profile_name
-  instance_name_tag     = "project-ec2-instance"
-  frontend_s3_bucket    = var.bucket_name
+# IAM Role for CodePipeline
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-service-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
+# IAM Role Policy Attachment for CodePipeline
+resource "aws_iam_role_policy_attachment" "codepipeline_attach" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineFullAccess"
+}
+
+# CodePipeline definition
+resource "aws_codepipeline" "pipeline" {
+  name     = "devsecops-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.artifact_bucket.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "SourceAction"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner      = var.github_owner
+        Repo       = var.github_repo
+        Branch     = var.github_branch
+        OAuthToken = var.github_token
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "BuildAction"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.ci_project.name
+      }
+    }
+  }
+}
+
+# EC2 instance to deploy after pipeline
+resource "aws_instance" "dev_node" {
+  ami           = var.ec2_ami
+  instance_type = var.instance_type
+  tags = {
+    Name = "DeployedAppServer"
+  }
+}
--- output.tf
+++ output.tf
@@ -1,28 +1,16 @@
-output "codebuild_project_name" {
-  description = "CodeBuild project name"
-  value       = var.project_name
+# output.tf - Outputs for monitoring and testing
+
+output "ec2_public_ip" {
+  description = "Public IP of EC2 instance"
+  value       = aws_instance.dev_node.public_ip
 }
-output "codepipeline_name" {
-  description = "CodePipeline name"
-  value       = module.codepipeline.pipeline_name
+
+output "pipeline_name" {
+  description = "Name of the AWS CodePipeline"
+  value       = aws_codepipeline.pipeline.name
 }
-output "s3_bucket_name" {
-  description = "S3 bucket used in pipeline"
-  value       = module.s3.bucket_name
-}
-output "codepipeline_role_arn" {
-  description = "Codepipeline role arn"
-  value       = module.iam.codepipeline_role_arn
-}
-output "codebuild_role_arn" {
-  description = "Codebuild role arn"
-  value       = module.iam.codebuild_role_arn
-}
-output "github_connection_name" {
-  description = "GitHub CodeStar connection name"
-  value       = module.iam.github_connection_name
-}
-output "github_codestar_connection_arn" {
-  description = "CodeStar connection arn"
-  value       = module.iam.github_codestar_connection_arn
+
+output "artifact_bucket_name" {
+  description = "S3 bucket for CodePipeline artifacts"
+  value       = aws_s3_bucket.artifact_bucket.id
 }
